b39e72a19adb7b53757648e5da2a54e67122c1d3
Morten Beuchert <mortenbeuchert@gmail.com>
2018-06-24 20:22:06 +0200

    Add local storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -23,7 +23,15 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+      return b.date - a.date;
+    }));
   }
 
   setFormLog(log: Log) {
@@ -32,6 +40,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log) {
@@ -42,6 +53,9 @@ export class LogService {
     });
 
     this.logs.unshift(log);
+
+    // Update local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log: Log) {
@@ -50,6 +64,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    // Delete log from local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   clearState() {
